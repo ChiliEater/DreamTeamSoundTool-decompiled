dd24c06276a4c43b05f2c0e0c4a8f90755eb56ac
costajon <costajon@students.zhaw.ch>
2022-08-22 13:43:15 +0200

    Complete converter functions and documentation

--- LibDTSoundData/SoundDataArc.cs
+++ LibDTSoundData/SoundDataArc.cs
@@ -1,3 +1,4 @@
+using System.Dynamic;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,19 +13,40 @@ namespace DTSoundData
     /// <typeparam name="int">The Sound type ID</typeparam>
     /// <typeparam name="string">The sound type's name</typeparam>
     /// <returns></returns>
-    public enum soundTypes : int
+    public enum SoundTypes : int
     {
         WAVE, SE, SEB, STRSE, STRBGM, STRVOICE
     }
 
+    /// <summary>
+    /// This class provides functions to read and convert an ARC-File into a generally usable format
+    /// </summary>
     public class SoundDataArc
     {
         /// <summary>
+        /// Offset to (presumably) skip the header info
+        /// </summary>
+        private const int OFFSET_PAD = 32;
+        /// <summary>
+        /// Multiplier to jump to an entry
+        /// </summary>
+        private const int OFFSET_MULTIPLIER = 16;
+        /// <summary>
+        /// The file extension of the output files
+        /// </summary>
+        private const string FILE_EXTENSION = ".rsd";
+
+        /// <summary>
         /// Relative path to the ARC-File
         /// </summary>
-        public string path;
-        
-        public Dictionary<int, SoundDataArcTable> tables = new Dictionary<int, SoundDataArcTable>();
+        private string path;
+
+        /// <summary>
+        /// Dictionary that holds all ARC-Tables
+        /// </summary>
+        /// <typeparam name="int">ID of the table</typeparam>
+        /// <typeparam name="SoundDataArcTable">The table</typeparam>
+        private Dictionary<int, SoundDataArcTable> tables = new Dictionary<int, SoundDataArcTable>();
 
 
         /// <summary>
@@ -33,21 +55,92 @@ namespace DTSoundData
         /// <param name="filename">Relative path to the ARC-File</param>
         public SoundDataArc(string filename)
         {
-            this.path = filename;
-            this.parseArchive();
+            path = filename;
+            parseArchive();
         }
 
-        private void parseArchive()
-        {
-
-        }
-
+        /// <summary>
+        /// Extracts the contents of the ARC-File onto disk
+        /// </summary>
+        /// <param name="outFolder">The desired output folder</param>
         public void extractArchive(string outFolder)
         {
+            using BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open));
 
+            foreach (KeyValuePair<int, SoundDataArcTable> table in tables)
+            {
+                string str = Path.Combine(outFolder, ((SoundTypes) table.Key).ToString());
+                if (!Directory.Exists(str))
+                {
+                    Directory.CreateDirectory(str);
+                }
+
+                foreach(KeyValuePair<int, SoundDataFile> file in table.Value.fileList)
+                {
+                    reader.BaseStream.Seek(table.Value.tableSize + file.Value.offset, SeekOrigin.Begin);
+                    
+                    using BinaryWriter writer = new BinaryWriter(File.Open(Path.Combine(str, file.Value.name + FILE_EXTENSION), FileMode.Create));
+                    writer.Write(reader.ReadBytes(file.Value.size));
+                    writer.Close();
+                }
+            }
+            reader.Close();
         }
 
-        public bool buildArchive(string sourceFolder, string newSoundArchive)
+        /// <summary>
+        /// Reads metadata from the set ARC-File
+        /// </summary>
+        private void parseArchive()
+        {
+            using BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open));
+            long length = new FileInfo(path).Length;
+
+            while (reader.BaseStream.Position < length)
+            {
+                // Get ARC-Table info
+                SoundDataArcTable arcTable = new SoundDataArcTable();
+                int tableKey = reader.ReadInt32();
+                arcTable.entryCount = reader.ReadInt32();
+                arcTable.tableSize = reader.ReadInt32();
+                arcTable.relativeFileDataOffset = reader.ReadInt32();
+                arcTable.tableStartOffset = reader.ReadInt32();
+                arcTable.stringTableSize = reader.ReadInt32();
+                
+                // Get file infos
+                for (int entryKey = 0; entryKey < arcTable.entryCount; ++entryKey)
+                {
+                    reader.BaseStream.Seek(arcTable.tableStartOffset + OFFSET_PAD + entryKey * OFFSET_MULTIPLIER, SeekOrigin.Begin);
+                    SoundDataFile file = new SoundDataFile();
+                    file.fileID = reader.ReadInt32();
+                    file.size = reader.ReadInt32();
+                    file.offset = reader.ReadInt32();
+                    arcTable.fileList.Add(entryKey, file);
+                }
+
+                // Reset position
+                reader.BaseStream.Seek(arcTable.tableStartOffset + OFFSET_PAD + arcTable.entryCount * OFFSET_MULTIPLIER, SeekOrigin.Begin);
+
+                // Trim whitespaces
+                for (int entryKey = 0; entryKey < arcTable.entryCount; ++entryKey)
+                {
+                    arcTable.fileList[entryKey].name = reader.ReadString().TrimEnd(new char[1]);
+                }
+
+                // Add table to dictionary and advance stream
+                tables.Add(tableKey, arcTable);
+                reader.BaseStream.Seek(arcTable.tableStartOffset + arcTable.tableSize, SeekOrigin.Begin);
+            }
+
+            reader.Close();
+        }
+
+        /// <summary>
+        /// Unused function from the original assembly. Stubbed, just in case
+        /// </summary>
+        /// <param name="sourceFolder"></param>
+        /// <param name="newSoundArchive"></param>
+        /// <returns></returns>
+        private bool buildArchive(string sourceFolder, string newSoundArchive)
         {
             return false;
         }
