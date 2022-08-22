6351d7de1b019e615353e3f49495c05e19d61428
costajon <costajon@students.zhaw.ch>
2022-08-22 13:42:40 +0200

    Add documentation to data classes

--- LibDTSoundData/SoundDataArcTable.cs
+++ LibDTSoundData/SoundDataArcTable.cs
@@ -1,12 +1,35 @@
 namespace DTSoundData
 {
+    /// <summary>
+    /// A data class used to store an ARC-Table
+    /// </summary>
     public class SoundDataArcTable
     {
+        /// <summary>
+        /// The amount of entries
+        /// </summary>
         public int entryCount { get; set; }
+        /// <summary>
+        /// THe size of the table in (presumably) bytes
+        /// </summary>
         public int tableSize { get; set; }
+        /// <summary>
+        /// (Presumably) offset from one file to another within the ARC-File)
+        /// </summary>
         public int relativeFileDataOffset { get; set; }
+        /// <summary>
+        /// The point where this table starts
+        /// </summary>
         public int tableStartOffset { get; set; }
+        /// <summary>
+        /// Size of the name table
+        /// </summary>
         public int stringTableSize { get; set; }
+        /// <summary>
+        /// A map containing the sound files
+        /// </summary>
+        /// <typeparam name="int">The ID</typeparam>
+        /// <typeparam name="SoundDataFile">The File</typeparam>
         public Dictionary<int, SoundDataFile> fileList = new Dictionary<int, SoundDataFile>();
     }
 }
--- LibDTSoundData/SoundDataFile.cs
+++ LibDTSoundData/SoundDataFile.cs
@@ -1,10 +1,25 @@
 namespace DTSoundData
 {
+    /// <summary>
+    /// This data class holds info about a sound file
+    /// </summary>
     public class SoundDataFile
     {
+        /// <summary>
+        /// The file ID
+        /// </summary>
         public int fileID { get; set; }
+        /// <summary>
+        /// The size of the file
+        /// </summary>
         public int size { get; set; }
+        /// <summary>
+        /// The position of the file in the ARC-File
+        /// </summary>
         public int offset { get; set; }
+        /// <summary>
+        /// The name of this file as the string table indicates
+        /// </summary>
         public string? name { get; set; }
     }
 }
