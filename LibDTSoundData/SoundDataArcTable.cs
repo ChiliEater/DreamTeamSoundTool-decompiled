ffeefd19d24ea5a8c6f6dcb58c814440a5e86b5f
costajon <costajon@students.zhaw.ch>
2022-08-22 11:15:11 +0200

    Rename table info

--- LibDTSoundData/SoundDataArcTable.cs
+++ LibDTSoundData/SoundDataArcTable.cs
@@ -4,7 +4,7 @@ namespace DTSoundData
     {
         public int entryCount { get; set; }
         public int tableSize { get; set; }
-        public int relFileDataOffset { get; set; }
+        public int relativeFileDataOffset { get; set; }
         public int tableStartOffset { get; set; }
         public int stringTableSize { get; set; }
         public Dictionary<int, SoundDataFile> fileList = new Dictionary<int, SoundDataFile>();
